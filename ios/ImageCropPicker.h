f18c68bb1119c3fee4fd187e9a3f4be43507cd54
projectzerorn <jackzhou.projectzero@gmail.com>
2017-11-13 02:09:14 -0600

    Update ImageCropPicker.h

--- ios/ImageCropPicker.h
+++ ios/ImageCropPicker.h
@@ -10,13 +10,8 @@
 
 #import <Foundation/Foundation.h>
 
-#if __has_include("RCTBridgeModule.h")
-#import "RCTBridgeModule.h"
-#import "RCTImageLoader.h"
-#else
 #import <React/RCTBridgeModule.h>
 #import <React/RCTImageLoader.h>
-#endif
 
 #import "QBImagePicker/QBImagePicker.h"
 #import "RSKImageCropper/RSKImageCropper.h"
